57ba9a12ac88e01d7d45adb3a651da0e877f5e5a
DuskyShelf <dupsdale@gmail.com>
2016-04-01 06:12:50 +0100

    adds template api call test

--- js/controller.js
+++ js/controller.js
@@ -3,5 +3,17 @@ programListings.controller('ProgramListingsController', [function() {
   var self = this;
   self.hello = "TEST";
 
-  console.log(self.hello)
+  var programs = [
+    {
+      title: "Abadas",
+      images: { standard: "http://ichef.bbci.co.uk/images/ic/192x108/p02c72z7.jpg" }
+    },
+    {
+      title: "a b c",
+      images: { standard: "http://ichef.bbci.co.uk/images/ic/192x108/p02g3r3r.jpg" }
+    }
+  ];
+
+  self.apiCall = { results: programs }
+
 }]);
--- test/programListingController.spec.js
+++ test/programListingController.spec.js
@@ -8,8 +8,27 @@ describe('ProgramListingsController', function() {
     ctrl = $controller('ProgramListingsController');
   }));
 
-  it('initialises with default filter settings', function() {
+  it('initialises with the program listings controller', function() {
     expect(ctrl).toBeDefined();
+  });
+
+  describe('when calling the api', function() {
+
+    var programs = [
+      {
+        title: "Abadas",
+        images: { standard: "http://ichef.bbci.co.uk/images/ic/192x108/p02c72z7.jpg" }
+      },
+      {
+        title: "a b c",
+        images: { standard: "http://ichef.bbci.co.uk/images/ic/192x108/p02g3r3r.jpg" }
+      }
+    ];
+
+    it('displays search results', function() {
+      expect(ctrl.apiCall.results).toEqual(programs);
+    });
+
   });
 
 });
