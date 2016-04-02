a8988df5b27b79098e6883b0c499dd5b490a9daa
DuskyShelf <dupsdale@gmail.com>
2016-04-02 22:33:10 +0100

    adds pagination links

--- js/controller.js
+++ js/controller.js
@@ -1,11 +1,14 @@
 programList.controller('ProgramListController', ['Api', '$routeParams', function(Api, $routeParams) {
 
   var self = this;
-  self.letterList = [];
-  self.listing = [];
+  self.programs = [];
   self.letter = $routeParams.letter;
   self.pageNum = $routeParams.number;
+  self.letterList = [];
+  self.paginationList = [];
 
+
+  // generate alphabet filter link data
   var alphabetArray = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
   alphabetArray.push("0-9");
 
@@ -17,24 +20,39 @@ programList.controller('ProgramListController', ['Api', '$routeParams', function
     }
   });
 
+  // call api with relevant parameters
   self.apiCall = function(letter, pageNum) {
     Api.query(letter, pageNum)
       .then(function(response) {
         self.data = response.data.atoz_programmes;
-        self.listing = response.data.atoz_programmes.elements;
+        self.programs = response.data.atoz_programmes.elements;
+        populatePaginationList(self.data);
       })
   };
 
+  self.apiCall(self.letter, self.pageNum);
+
+
+  // generate pagination link data
+  var pageCount = function(data) {
+    return Math.ceil(data.count / 20);
+  };
+
+  var populatePaginationList = function(data) {
+    for (var i=1; i <= pageCount(data); i++) {
+      self.paginationList.push({
+        number: i,
+        id: "page-" + i,
+        url: "/#/" + $routeParams.letter + "/" + i
+      })
+    };
+  };
+
+
+  // modify returned image urls to include sizing information
   self.parseImageUrl = function(url) {
     // possible sizes 192x108, 406x228, 560x315
     return url.replace("{recipe}", "192x108");
-  }
+  };
 
-  self.pageCount = function(data) {
-    return Math.ceil(data.count / 20);
-  }
-
-
-
-  self.apiCall(self.letter, self.pageNum);
 }]);
--- test/e2e/programListFeature.js
+++ test/e2e/programListFeature.js
@@ -22,10 +22,36 @@ describe('BBC Program List', function() {
     });
 
     it('navigates to "0-9" filter correctly', function() {
-      var bButton = element(by.id('filter-0-9'));
-      bButton.click()
+      var numButton = element(by.id('filter-0-9'));
+      numButton.click()
       expect(browser.getCurrentUrl()).toEqual('http://localhost:8080/#/0-9/1');
     });
 
   });
+
+  describe('Pagination', function() {
+
+    it('navigates to 2nd page correctly', function() {
+      var page2Button = element(by.id('page-2'));
+      page2Button.click()
+      expect(browser.getCurrentUrl()).toEqual('http://localhost:8080/#/a/2');
+    });
+
+    it('navigates to 3rd page correctly', function() {
+      var page3Button = element(by.id('page-3'));
+      page3Button.click()
+      expect(browser.getCurrentUrl()).toEqual('http://localhost:8080/#/a/3');
+    });
+
+    it('navigates to 2nd page correctly after changing letter filter', function() {
+      var bButton = element(by.id('filter-B'));
+      bButton.click()
+      var page2Button = element(by.id('page-2'));
+      page2Button.click()
+      expect(browser.getCurrentUrl()).toEqual('http://localhost:8080/#/b/2');
+    });
+
+  });
+
+
 });
